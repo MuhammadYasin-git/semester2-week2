3e2c3a1918f210526fd7251d43c251e44a07402c
MuhammadYasin-git <cnnj3831@leeds.ac.uk>
2026-02-06 12:20:18 +0000

    Task_1 Complete

--- session_2/task1.py
+++ session_2/task1.py
@@ -27,11 +27,72 @@ def menu():
         choice = input("Enter your choice: ").upper()
     return choice
 
+def all_product(db):
+    '''
+    Shows all product Names
+    '''
+    query = "SELECT category FROM products;"
+    cursor = db.execute(query)
+    # where we know we should have multiple results, we can iterate over the cursor.
+    for product in cursor:
+        print(f"Name: {product['category']}")
+
+def count_customer(db):
+    '''
+    Shows number of Customers
+    '''
+    query = "SELECT COUNT(customer_id) FROM customers;"
+    cursor = db.execute(query)
+    # where we know we should have multiple results, we can iterate over the cursor.
+    for customer in cursor:
+        print(f"Number of Customers: {customer[0]}")
+
+def view_order_customer(db):
+    choice = ""
+    while(choice==""):
+        choice = input("Enter Customer Email ")
+    query = '''
+            SELECT orders.order_id
+            FROM orders JOIN customers
+            ON customers.customer_id = orders.customer_id
+            where customers.email =?;
+            '''
+    cursor = db.execute(query, (choice,))
+    for customer in cursor:
+        print(f"Order: {customer[0]}")
+
+def price_less_2(db):
+    '''
+    Shows all product Names
+    '''
+    query = '''SELECT DISTINCT name , price
+            FROM products
+            WHERE price < 2
+            '''
+    cursor = db.execute(query)
+    # where we know we should have multiple results, we can iterate over the cursor.
+    for product in cursor:
+        print(f"Products: {product[0]}, Price: {product[1]} ")
+
+
 
 def main():
 
     db = get_connection()
 
+    while 1:
+        choice = menu()
+        match(choice):
+            case "1":
+                all_product(db)
+            case "2":
+                count_customer(db)
+            case "3":
+                view_order_customer(db)
+            case "4":
+                price_less_2(db)
+            case "Q":
+                exit()
     db.close()
 
 
